3818ab480691bc0c2f34be6184e9b1b5c313a21a
Leo Sek <1808860623@qq.com>
2023-05-02 15:57:42 +0800

    feat: vercel.json

--- service/src/index.ts
+++ service/src/index.ts
@@ -14,8 +14,9 @@ app.use(express.json())
 
 app.all('*', (_, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'authorization, Content-Type')
-  res.header('Access-Control-Allow-Methods', '*')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Headers', 'Authorization,Content-Type,Accep,Origin,User-Agent,DNT,Cache-Control,X-Mx-ReqToken,X-Requested-With')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,DELETE')
   next()
 })
 
